d8e21854e7e9169b0754910b6441afdd9cb06ae5
sharan-lh <sharan.hebbar@gmail.com>
2020-03-31 20:33:47 +0530

    Updates

---
+++ precache-manifest.aa30e9f8a49b91223a6518ddd41033be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802f8864f8309b28557b6e448b65ca76",
+    "url": "/React-TodoApp/index.html"
+  },
+  {
+    "revision": "5779f5813ac0fc5bfeee",
+    "url": "/React-TodoApp/static/js/2.59cdb716.chunk.js"
+  },
+  {
+    "revision": "0e4c47b858ae6f79e735b78831b85dc8",
+    "url": "/React-TodoApp/static/js/2.59cdb716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18832b1faad75ce9f8c1",
+    "url": "/React-TodoApp/static/js/main.db45f65a.chunk.js"
+  },
+  {
+    "revision": "0df82ee22870048dd07f",
+    "url": "/React-TodoApp/static/js/runtime-main.dbadc765.js"
+  }
+]);
